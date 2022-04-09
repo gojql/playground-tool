30fd11662b8dd6d3a4c1df13fd1a0715724cf1ff
Soyuz <mathur.soyuz@gmail.com>
2022-04-09 15:10:45 +0530

    Remove vue.config.js file as no customizations are required for now

--- vue.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    runtimeCompiler: true,
-    // or
-    chainWebpack: (config) => {
-        config.resolve.alias
-            .set("vue$", "vue/dist/vue.esm.js");
-    },
-};
